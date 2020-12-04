70763c902d7634e835c7272350f527fdc0dc5072
tgray <graytc1985@gmail.com>
2020-12-04 10:55:21 -0500

    deleted SNS, SQS

--- cdk_projects/cdk_projects_stack.py
+++ cdk_projects/cdk_projects_stack.py
@@ -1,9 +1,5 @@
 from aws_cdk import (
-    aws_iam as iam,
-    aws_sqs as sqs,
-    aws_sns as sns,
-    aws_sns_subscriptions as subs,
-    core
+    core,
 )
 
 
@@ -11,14 +7,3 @@ class CdkProjectsStack(core.Stack):
 
     def __init__(self, scope: core.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
-        queue = sqs.Queue(
-            self, "CdkProjectsQueue",
-            visibility_timeout=core.Duration.seconds(300),
-        )
-
-        topic = sns.Topic(
-            self, "CdkProjectsTopic"
-        )
-
-        topic.add_subscription(subs.SqsSubscription(queue))
